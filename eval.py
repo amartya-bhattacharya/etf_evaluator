4d2d099f71bb739c144ebbd0ba466e8260a7f42f
Amartya Bhattacharya <amartyacharya@gmail.com>
2023-08-21 19:54:46 -0500

    improved robustness

--- eval.py
+++ eval.py
@@ -1,46 +1,75 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from scrape import scrape_data
+import cProfile
+import pstats
+from io import StringIO
+
+
+def process_ticker(ticker):
+    data, benchmark_data = fetch_data(ticker.strip())
+    premium_discount, valuation = analyze_etf(data, benchmark_data)
+
+    analysis = (
+        f"\n{ticker.strip()}:",
+        f"Trading at a {premium_discount} to its NAV.",
+        f"It is {valuation} compared to the benchmark."
+    )
+
+    return analysis
 
 
 def fetch_data(ticker):
-    # Using the scrape_data function from scrape.py to fetch real data for the given ticker
-    base_url = "https://investor.vanguard.com/investment-products/etfs/profile/"
-    url = base_url + ticker.strip()
-    data, benchmark_data = scrape_data(url)
-    print(f"Data for {ticker.strip()} fetched successfully!")
-    print(f"ETF Data: {data}")
-    print(f"Benchmark Data: {benchmark_data}")
+    # print(f"Fetching data for {ticker.strip()}...")
+    data, benchmark_data = scrape_data(ticker)
+    # print(f"{ticker} ETF Data: {data}")
+    # print(f"Benchmark Data: {benchmark_data}")
     return data, benchmark_data
 
 
 def analyze_etf(data, benchmark_data):
-    # Compare market price to NAV
-    if data["market_price"] > data["NAV"]:
+    if data["market_price"] is None or data["NAV"] is None:
+        premium_discount = "N/A"
+    elif data["market_price"] > data["NAV"]:
         premium_discount = "premium"
     elif data["market_price"] < data["NAV"]:
         premium_discount = "discount"
     else:
         premium_discount = "fair value"
 
-    # Compare P/E and P/B to benchmark
-    valuation = "fairly valued"
-    if data["PE_ratio"] > benchmark_data["PE_ratio"] or data["PB_ratio"] > benchmark_data["PB_ratio"]:
+    if None in [data["PE_ratio"], benchmark_data["PE_ratio"], data["PB_ratio"], benchmark_data["PB_ratio"]]:
+        valuation = "N/A"
+    elif data["PE_ratio"] > benchmark_data["PE_ratio"] or data["PB_ratio"] > benchmark_data["PB_ratio"]:
         valuation = "overvalued"
     elif data["PE_ratio"] < benchmark_data["PE_ratio"] or data["PB_ratio"] < benchmark_data["PB_ratio"]:
         valuation = "undervalued"
+    else:
+        valuation = "fairly valued"
 
     return premium_discount, valuation
 
 
 def main():
-    tickers = input("Enter the ETF tickers separated by commas: ").split(",")
+    # tickers = input("Enter the ETF tickers separated by commas: ").split(",")
+    tickers = 'VTI,VIOG,VGT,VFH,VHT,VNQ,VDE,VCR,VXUS'.split(",")
 
-    for ticker in tickers:
-        data, benchmark_data = fetch_data(ticker.strip())
-        premium_discount, valuation = analyze_etf(data, benchmark_data)
+    profiler = cProfile.Profile()
+    s = StringIO()
 
-        print(f"\nETF {ticker.strip()} Analysis:")
-        print(f"Trading at a {premium_discount} to its NAV.")
-        print(f"It is {valuation} based on its P/E and P/B ratios compared to the benchmark.\n")
+    profiler.enable()  # Start profiling
+
+    print("Fetching data...")
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(process_ticker, tickers))
+
+    for result in results:
+        print(*result)
+
+    profiler.disable()  # Stop profiling
+
+    stats = pstats.Stats(profiler, stream=s).sort_stats("time")
+    stats.print_stats(0)
+    print(s.getvalue())  # Print the contents of the StringIO object
 
 
 if __name__ == "__main__":
--- scrape.py
+++ scrape.py
@@ -1,39 +1,88 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
-def scrape_data(target_url):
-    driver = webdriver.Chrome()  # Make sure ChromeDriver is installed and in PATH
+def scrape_data(target_ticker):
+    target_url = build_url(target_ticker)
+    options = Options()
+    # options.page_load_strategy = 'eager'  # not working
+    options.add_argument("--headless=new")  # Run Chrome in headless mode
+    options.add_argument("--disable-gpu")  # Disable GPU acceleration
+    options.add_argument("--no-sandbox")  # Disable sandbox mode
+
+    driver = webdriver.Chrome(options=options)  # Make sure ChromeDriver is installed and in PATH
     driver.get(target_url)
-    # driver.implicitly_wait(0.5)
+
+    # Explicit waits for each element
+    wait = WebDriverWait(driver, timeout=10)
 
     # Extracting Current Market Price (IIV), Closing Market Price, and Closing NAV Price
-    iiv = float(
-        driver.find_element(
-            by=By.CSS_SELECTOR, value='h2[data-rpa-tag-id="priceMarket"]').text.replace('$', ''))
-    market_price = float(
-        driver.find_element(
-            by=By.CSS_SELECTOR, value='h4[data-rpa-tag-id="priceMarket"]').text.replace('$', ''))
-    nav_price = float(driver.find_element(
-        by=By.CSS_SELECTOR, value='h4[data-rpa-tag-id="priceNAV"]').text.replace('$', ''))
+    try:
+        # Wait until the IIV value is not '—'
+        iiv_element = wait.until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, 'h2[data-rpa-tag-id="priceMarket"]')))
 
-    # Extracting P/E Ratio for data and benchmark_data
-    pe_ratio_data = float(
-        driver.find_element(
-            by=By.CSS_SELECTOR, value='.equity-characteristics:nth-child(6) td:nth-child(2)').text.replace(
-            'x', ''))
-    pe_ratio_benchmark = float(
-        driver.find_element(
-            by=By.CSS_SELECTOR, value='.equity-characteristics:nth-child(6) td:nth-child(3)').text.replace(
-            'x', ''))
+        # Additional wait for the text to be different from '—'
+        wait.until(lambda driver: iiv_element.text != '—')
 
-    # Extracting P/B Ratio for data and benchmark_data
-    pb_ratio_data = float(
-        driver.find_element(
-            by=By.CSS_SELECTOR, value='.equity-characteristics:nth-child(7) td:nth-child(2)').text.replace('x', ''))
-    pb_ratio_benchmark = float(
-        driver.find_element(
-            by=By.CSS_SELECTOR, value='.equity-characteristics:nth-child(7) td:nth-child(3)').text.replace('x', ''))
+        # Extracting the updated IIV
+        iiv = float(iiv_element.text.replace('$', ''))
+    except Exception as e:
+        print(f"{target_ticker} Error scraping IIV: {str(e)}")
+        iiv = None
+
+    try:
+        market_price_element = wait.until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, 'h4[data-rpa-tag-id="priceMarket"]')))
+        market_price = float(market_price_element.text.replace('$', ''))
+    except Exception as e:
+        print(f"{target_ticker} Error scraping market price: {str(e)}")
+        market_price = None
+
+    try:
+        nav_price_element = wait.until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, 'h4[data-rpa-tag-id="priceNAV"]')))
+        nav_price = float(nav_price_element.text.replace('$', ''))
+    except Exception as e:
+        print(f"{target_ticker} Error scraping NAV price: {str(e)}")
+        nav_price = None
+
+    # Extracting P/E Ratio for etf data and benchmark_data
+    try:
+        pe_ratio_data_element = wait.until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, '.equity-characteristics:nth-child(6) td:nth-child(2)')))
+        pe_ratio_data = float(pe_ratio_data_element.text.replace('x', ''))
+    except Exception as e:
+        print(f"{target_ticker} Error scraping PE ratio data: {str(e)}")
+        pe_ratio_data = None
+
+    try:
+        pe_ratio_benchmark_element = wait.until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, '.equity-characteristics:nth-child(6) td:nth-child(3)')))
+        pe_ratio_benchmark = float(pe_ratio_benchmark_element.text.replace('x', ''))
+    except Exception as e:
+        print(f"{target_ticker} Error scraping PE ratio benchmark: {str(e)}")
+        pe_ratio_benchmark = None
+
+    # Extracting P/B Ratio for etf data and benchmark_data
+    try:
+        pb_ratio_data_element = wait.until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, '.equity-characteristics:nth-child(7) td:nth-child(2)')))
+        pb_ratio_data = float(pb_ratio_data_element.text.replace('x', ''))
+    except Exception as e:
+        print(f"{target_ticker} Error scraping PB ratio data: {str(e)}")
+        pb_ratio_data = None
+
+    try:
+        pb_ratio_benchmark_element = wait.until(
+            ec.visibility_of_element_located((By.CSS_SELECTOR, '.equity-characteristics:nth-child(7) td:nth-child(3)')))
+        pb_ratio_benchmark = float(pb_ratio_benchmark_element.text.replace('x', ''))
+    except Exception as e:
+        print(f"{target_ticker} Error scraping PB ratio benchmark: {str(e)}")
+        pb_ratio_benchmark = None
 
     etf_data = {
         "NAV": nav_price,
@@ -51,3 +100,8 @@ def scrape_data(target_url):
     driver.quit()
 
     return etf_data, benchmark_data
+
+
+def build_url(ticker):
+    base_url = "https://investor.vanguard.com/investment-products/etfs/profile/"
+    return base_url + ticker.strip()
